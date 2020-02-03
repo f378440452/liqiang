0e76796f3f3bdc1ed2748049a7d325e5d4e0b672
flq <fengliqiangf@126.com>
2020-02-03 21:56:17 +0800

    项目原型

---
+++ src/test/java/cn/majiang/MajiangApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.majiang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MajiangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
